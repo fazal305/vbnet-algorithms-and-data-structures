ab5cb2cb305365e599e96f10d94c417fddb555f8
Fazal Abbas <fazalabbas2002@gmail.com>
2026-07-07 01:18:20 +0500

    Refactor job management code for clarity and efficiency

--- code in vs for number of jobs.vb
+++ code in vs for number of jobs.vb
@@ -1,17 +1,37 @@
+Option Strict On
 Imports System
+
 Module Program
-    Dim Jobs(99, 1) As Integer
+    Const MaxJobs As Integer = 100
+
+    Dim Jobs(MaxJobs - 1, 1) As Integer
     Dim NumberOfJobs As Integer
+
+    Sub Main()
+        Initialise()
+
+        AddJob(12, 10)
+        AddJob(526, 9)
+        AddJob(33, 8)
+        AddJob(12, 9)
+        AddJob(78, 1)
+
+        InsertionSort()
+        PrintArray()
+    End Sub
+
     Sub Initialise()
-        For X = 0 To 99
+        For X = 0 To MaxJobs - 1
             For Y = 0 To 1
                 Jobs(X, Y) = -1
             Next
         Next
+
         NumberOfJobs = 0
     End Sub
-    Sub AddJob(JobNumber, Priority)
-        If NumberOfJobs = 100 Then
+
+    Sub AddJob(JobNumber As Integer, Priority As Integer)
+        If NumberOfJobs = MaxJobs Then
             Console.WriteLine("Not added")
         Else
             Jobs(NumberOfJobs, 0) = JobNumber
@@ -20,43 +40,32 @@ Module Program
             Console.WriteLine("Added")
         End If
     End Sub
-    Sub Main()
-        Initialise()
-        AddJob(12, 10)
-        AddJob(526, 9)
-        AddJob(33, 8)
-        AddJob(12, 9)
-        AddJob(78, 1)
-        InsertionSort()
-        PrintArray()
-    End Sub
+
     Sub InsertionSort()
-        Dim Tempa As Integer
-        Dim Tempb As Integer
-        Dim Counter As Integer
-        Dim Placed As Boolean
-        For i = 1 To NumberOfJobs - 1
-            Tempa = Jobs(i, 0)
-            Tempb = Jobs(i, 1)
-            Counter = i
-            Placed = False
-            While (Counter > 0 And Not Placed)
-                If (Jobs(Counter - 1, 1) > Tempb) Then
-                    Jobs(Counter, 0) = Jobs(Counter - 1, 0)
-                    Jobs(Counter, 1) = Jobs(Counter - 1, 1)
-                    Counter = Counter - 1
+        For Index = 1 To NumberOfJobs - 1
+            Dim CurrentJobNumber As Integer = Jobs(Index, 0)
+            Dim CurrentPriority As Integer = Jobs(Index, 1)
+            Dim Position As Integer = Index
+            Dim Placed As Boolean = False
+
+            While Position > 0 And Not Placed
+                If Jobs(Position - 1, 1) > CurrentPriority Then
+                    Jobs(Position, 0) = Jobs(Position - 1, 0)
+                    Jobs(Position, 1) = Jobs(Position - 1, 1)
+                    Position = Position - 1
                 Else
                     Placed = True
                 End If
             End While
-            Jobs(Counter, 0) = Tempa
-            Jobs(Counter, 1) = Tempb
-        Next i
+
+            Jobs(Position, 0) = CurrentJobNumber
+            Jobs(Position, 1) = CurrentPriority
+        Next
     End Sub
+
     Sub PrintArray()
         For X = 0 To NumberOfJobs - 1
-            Console.WriteLine(Jobs(X, 0) & " priority " & Jobs(X,
-           1))
+            Console.WriteLine(Jobs(X, 0) & " priority " & Jobs(X, 1))
         Next
     End Sub
 End Module
